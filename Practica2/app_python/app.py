81b0c7a7894113409ecea72616e9c8b66c907f96
Luciano Xiquín <78318071+chox-1998@users.noreply.github.com>
2026-09-19 05:51:07 -0600

    json

--- Practica2/app_python/app.py
+++ Practica2/app_python/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
 from db import obtener_conexion
+import jsonify
 
 app = Flask(__name__)
 CORS(app)
